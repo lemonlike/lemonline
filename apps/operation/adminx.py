02f2a3bc0c6b750ceb17152a587aba34fff0faf8
Lemon <962650082@qq.com>
2017-02-13 20:44:30 +0800

    Add new codes

--- apps/operation/adminx.py
+++ apps/operation/adminx.py
@@ -19,8 +19,25 @@ class CourseCommentsAdmin(object):
     list_filter = ['user__username', 'course__name', 'comments', 'add_time']
 
 
-# class UserFavoriteAdmin(object):
+class UserFavoriteAdmin(object):
+    list_display= ['user', 'fav_id', 'fav_type', 'add_time']
+    search_fields = ['user__username', 'fav_id', 'fav_type']
+    list_filter = ['user__username', 'fav_id', 'fav_type', 'add_time']
 
+
+class UserMessageAdmin(object):
+    list_display = ['name', 'message', 'has_read', 'add_time']
+    search_fields = ['name', 'message', 'has_read']
+    list_filter = ['name', 'message', 'has_read', 'add_time']
+
+
+class UserCourseAdmin(object):
+    list_display = ['user', 'course', 'add_time']
+    search_fields = ['user__username', 'course__name']
+    list_filter = ['user__username', 'course__name', 'add_time']
 
 xadmin.site.register(UserAsk, UserAskAdmin)
 xadmin.site.register(CourseComments, CourseCommentsAdmin)
+xadmin.site.register(UserFavorite, UserFavoriteAdmin)
+xadmin.site.register(UserMessage, UserMessageAdmin)
+xadmin.site.register(UserCourse,UserCourseAdmin)
